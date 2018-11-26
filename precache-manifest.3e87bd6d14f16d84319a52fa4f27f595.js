600a79d8ad1fb8b2f873b9cb06944f04423257a4
Carrji22 <carrjim22@gmail.com>
2018-11-26 17:20:03 -0600

    mi primer commit

---
+++ precache-manifest.3e87bd6d14f16d84319a52fa4f27f595.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a7a8c7f8056784af5872",
+    "url": "/static/js/main.a7a8c7f8.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "a7a8c7f8056784af5872",
+    "url": "/static/css/main.ee15c11e.chunk.css"
+  },
+  {
+    "revision": "fe16b703251053524991de7e0347f7ec",
+    "url": "/index.html"
+  }
+];
